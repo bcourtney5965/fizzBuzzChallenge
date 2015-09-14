4e5eeb438eef175ceeb290a2c9135bb7d327773e
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-09-13 21:49:12 -0500

    updated files

---
+++ fizzBuzzChallenge.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	// Determining what to append
+	// Defining the function
+	function fizzBuzzfun(num) {
+		for (var i = 1; i <= num; i++) {
+			if (i % 15 === 0) {
+				$("#fizz").append("<li>FizzBuzz</li>")
+			} else if (i % 3 === 0) {
+				$("#fizz").append("<li>Fizz</li>");
+			} else if (i % 5 === 0) {
+				$("#fizz").append("<li>Buzz</li>");
+			} else {
+				$("#fizz").append("<li>" + i + "</li>");
+			}
+		}
+	}
+
+	alert("Let's get started!");
+
+	// Invoking the function
+	fizzBuzzfun(100);
+});
